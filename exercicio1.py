bb3631048734a0addea22639440a7f5463a41fd4
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-08-14 07:41:27 -0300

    tentativa 01 de entrega da atividade

--- exercicio1.py
+++ exercicio1.py
@@ -1,26 +1,13 @@
-#  Se achar necessario, faça import de outras bibliotecas
-
-
-
+# Maurício de Azevedo Neto
 
 
 # Crie a função que será avaliada no exercício aqui
+def conta_palavras_unicas(frase):
+    frase = frase.lower()
+    palavras = frase.split()
+    palavras_unicas = set(palavras)
 
-
-
-
-
+    return len(palavras_unicas)
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
--- exercicio2.py
+++ exercicio2.py
@@ -1,26 +1,31 @@
-#  Se achar necessario, faça import de outras bibliotecas
-
-
-
+# Maurício de Azevedo Neto
 
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(str1, str2):
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
 
+    if len(str1) != len(str2):
+        return False
 
+    count1 = {}
+    count2 = {}
 
+    for caractere in str1:
+        if caractere in count1:
+            count1[caractere] += 1
+        else:
+            count1[caractere] = 1
 
+    for caractere in str2:
+        if caractere in count2:
+            count2[caractere] += 1
+        else:
+            count2[caractere] = 1
+
+    return count1 == count2
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
-
